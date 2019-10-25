9b410b434cba69596db70258c65e3ca2cbb1dae4
Amos Kendall <apkendall10@gmail.com>
2019-10-24 19:07:55 -0500

    Added exclusion feature

--- GetData.r
+++ GetData.r
@@ -1,8 +1,8 @@
-getData <-function(source="fanduel",excl=""){
+getData <-function(source="fanduel",excl=list("	")){
 	#initialize variables
-	defense = "D"
+	defense <- "D"
 	if(source=="draftkings"){
-		defense = "DST"
+		defense <- "DST"
 	}
 	data<-data.frame(optimal=list(1,2,3,4,5,6,7,8,9,10,11,12,13,14))
 	temp<-data.frame(optimal=list(1,2,3,4,5,6,7,8))
@@ -14,6 +14,7 @@ getData <-function(source="fanduel",excl=""){
 	#grab data from rotogrinders for each position and store in data
 	for(position in positionList){
 		site<-paste("https://rotogrinders.com/projected-stats/nfl-",position,".csv?site=",source,sep="")
+		#https://rotogrinders.com/projected-stats/nfl-rb.csv?site=draftkings
 		fpath<-paste("~/Documents/",position,".csv",sep="")
 		temp <- read.csv(site, header=FALSE)
 		#write.csv(temp, file = fpath)
@@ -27,15 +28,17 @@ getData <-function(source="fanduel",excl=""){
 	for(i in seq(1,6)){
 		for(j in seq(1,NROW(data))) {
 			data[j,col[i]]<-0
-			if (data[j,'Pos']==pos[i]) {data[j,col[i]]<-1}
+			if (data[j,'Pos']==pos[i]) {
+				data[j,col[i]]<-1
+				}
 			#handle corner cases in data
 			if (data[j,'Cost']==0) {
 				data[j,'Value']<-0
 				data[j,'Cost']<-5000
 			}
 			#handle exclusions if we want to play games that don't involve all days
-			if (data[j,'Team']==excl) {data[j,'Value'] <- 0}
-			if (data[j,'Opp']==excl) {data[j,'Value'] <- 0}
+			if (any(excl==data[j,'Team'])) {data[j,'Value'] <- 0}
+			if (any(excl == data[j,'Opp'])) {data[j,'Value'] <- 0}
 		}
 	}
 	#do some additional formatting
